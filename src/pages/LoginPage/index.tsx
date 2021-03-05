eab899a2728116701a40dbab96457fd031ef9458
Matheus Mendes <mattheus2015@yahoo.com.br>
2021-03-05 14:52:30 -0300

    feat: created intial state for login page

---
+++ src/pages/LoginPage/index.tsx
@@ -0,0 +1,14 @@
+import {
+    PageContainer
+} from './styles'
+import CommonInput from '../../components/CommonInput/index'
+
+const LoginPage = () => {
+    return (
+        <PageContainer>
+            <CommonInput />
+        </PageContainer>
+    )
+}
+
+export default LoginPage
