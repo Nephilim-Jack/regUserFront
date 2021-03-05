7a14d668a99c88d392b7da38c2fe17f3c357c30b
Matheus Mendes <mattheus2015@yahoo.com.br>
2021-03-05 14:51:46 -0300

    feat: Header style created

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #FF9500;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 64px;
+`
+
+export const HeaderMessage = styled.h1`
+    color: white;
+`
+
+export const PageWrapper = styled.div`
+    width: 100%;
+    margin-top: 64px;
+`
