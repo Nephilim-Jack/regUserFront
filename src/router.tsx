554536490a1c08535144a48a265603e8d6f5213f
Matheus Mendes <mattheus2015@yahoo.com.br>
2021-03-05 14:53:23 -0300

    feat: update router to reflect all changes

--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,23 @@ import {
     BrowserRouter, Route
 } from 'react-router-dom'
 import HomePage from './pages/HomePage/index'
+import LoginPage from './pages/LoginPage/index'
+import AccessLevelControl from './pages/ACL/index';
+import PageContext from './context/index'
+import Header from './components/Header/index';
 
 
 const Router = () => {
     return (
-        <BrowserRouter>
-            <Route path='/' component={HomePage} exact/>
-        </BrowserRouter>
+        <PageContext>
+            <Header />
+            <BrowserRouter>
+            <AccessLevelControl>
+                <Route path='/' component={HomePage} exact/>:
+            </AccessLevelControl>
+            <Route path='/login' component={LoginPage} exact/>
+            </BrowserRouter>
+        </PageContext>
     )
 }
 
