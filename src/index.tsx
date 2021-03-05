3f7bf5895e54c08dc0f25577f68cbd714d564c3d
Matheus Mendes <mattheus2015@yahoo.com.br>
2021-03-05 14:54:57 -0300

    feat: link to app color palette added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './router';
+// App color palette: https://coolors.co/ff7b00-ff8800-ff9500-ffa200-ffaa00-ffb700-ffc300-ffd000-ffdd00-ffea00
 
 ReactDOM.render(
   <React.StrictMode>
