918e0131c8b333e66ecb3658f9358105e0cef700
Matheus Mendes <mattheus2015@yahoo.com.br>
2021-03-08 11:20:23 -0300

    feat: login user function created

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { UserData, UserLoginData } from './types'
+import { UserData, UserLoginData, TokenData } from './types'
 
 const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/'
@@ -11,8 +11,21 @@ export const createUser = async (userData: UserData) => {
     })
 }
 
-export const getUsername = async (userLogin: UserLoginData) => {
-    await api.post<UserData>('v1/users/getUserLogin/', userLogin).then(response => {
+const getUsername = async (userLogin: UserLoginData) => {
+    return await api.post<UserData>('v1/users/getUserLogin/', userLogin).then(response => {
         return response.data.username
+    })
+}
+
+export const loginUser = async (userLogin: UserLoginData) => {
+    const loginData = {
+        username: await getUsername(userLogin),
+        password: userLogin.password,
+    }
+    return await api.post<TokenData>('v1/token/login/', loginData).then(response => {
+        return {
+            access: response.data.access,
+            refresh: response.data.refresh
+        } as TokenData
     })
 }
