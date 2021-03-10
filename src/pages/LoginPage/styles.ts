21b2fa6fa7c8221f17a7e87dcfb39ef914a4ebe1
Matheus Mendes <mattheus2015@yahoo.com.br>
2021-03-10 20:26:03 -0300

    feat: styles for login page

--- src/pages/LoginPage/styles.ts
+++ src/pages/LoginPage/styles.ts
@@ -13,6 +13,33 @@ export const LoginContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: 36rem;
+    justify-content: space-between;
+    height: 20rem;
     width: 24rem;
+    margin-top: 32px;
+`
+
+export const StyledOption = styled.option`
+`
+
+export const StyledSelection = styled.select`
+    color: #ffa200;
+    font-size: 1.1rem;
+    border: none;
+    outline: none;
+`
+
+export const LoginButton = styled.button`
+    width: 8rem;
+    border: 1px solid white;
+    border-radius: 8px;
+    outline: none;
+    cursor: pointer;
+    background-color: #FF7B00;
+    color: white;
+    font-size: 1.4rem;
+    :hover {
+        background-color: #B85900;
+        color: lightgray;
+    }
 `
