c559c26cc88e3defb1d384137edf03a6717d6b3f
Matheus Mendes <mattheus2015@yahoo.com.br>
2021-03-11 15:04:19 -0300

    feat: homepage final verssion

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,30 @@
 import {
-    PageContainer
+    PageContainer, CustomButton, PresentationText
 } from './styles'
+import {deleteUserSession, accessCookieName, refreshCookieName} from '../../utils/sessions'
+import {useHistory} from 'react-router-dom'
+import {useNameContext} from '../../context/index'
 
 const HomePage = () => {
+    const history = useHistory()
+    const {setUserName} = useNameContext()
+    const logoutUser = () => {
+        if (accessCookieName && refreshCookieName){
+            deleteUserSession(accessCookieName)
+            deleteUserSession(refreshCookieName)
+            history.push('login')
+            setUserName('Visitante')
+        }
+    }
     return (
         <PageContainer>
-            <p>Hello World</p>
+            <PresentationText>Infelizmente nosso sistema ainda não está concluido por isso só temos esses dois Botões</PresentationText>
+            <CustomButton onClick={logoutUser}>
+                Logout
+            </CustomButton>
+            <CustomButton onClick={() => history.push('editUser')}>
+                Alterar dados cadastrais
+            </CustomButton>
         </PageContainer>
     )
 }
--- src/pages/HomePage/styles.ts
+++ src/pages/HomePage/styles.ts
@@ -2,4 +2,30 @@ import styled from 'styled-components'
 
 export const PageContainer = styled.div`
     display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`
+
+export const CustomButton = styled.button`
+    margin-top: 40px;
+    width: fit-content;
+    padding: 8px;
+    border: 1px solid white;
+    border-radius: 8px;
+    outline: none;
+    cursor: pointer;
+    background-color: #FF7B00;
+    color: white;
+    font-size: 1.4rem;
+    :hover {
+        background-color: #B85900;
+        color: lightgray;
+    }
+`
+
+export const PresentationText = styled.h2`
+    text-align: center;
 `
